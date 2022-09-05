6911c9adba8abbb06eb2f0d7260c60cc68cfe7ae
Feris-Alexsy <ahmadlamborghiniavetador>
2022-09-05 08:00:32 +0700

    Initial commit

---
+++ src/SwichWithoutBreak.java
@@ -0,0 +1,19 @@
+public class SwichWithoutBreak {
+    public static void main(String[] args) {
+        char input = 'C';
+        int output = 0;
+        switch (input) {
+            case 'A':
+                output++;
+            case 'C':
+                output++;
+            case 'D':
+                output++;
+            case 'E':
+                output++;
+            default:
+                output++;
+        }
+        System.out.println("Nilai akhirnya adalah " + output);
+    }
+}
